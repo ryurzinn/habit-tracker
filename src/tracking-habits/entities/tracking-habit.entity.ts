773fdb9843d3ccf35a6a447ee8a554370050fff6
ryurzinn <chinwenwencha47@gmail.com>
2025-08-31 13:50:35 -0300

    entity tracking-habits terminada

--- src/tracking-habits/entities/tracking-habit.entity.ts
+++ src/tracking-habits/entities/tracking-habit.entity.ts
@@ -1,1 +1,27 @@
-export class TrackingHabit {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity('tracking-habits')
+export class TrackingHabit {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    //TODO: Se llena automaticamente cuando se crea un habito nuevo
+    @CreateDateColumn()
+    createdDate: Date;
+
+    //TODO: Setear automaticamente como completado
+    @Column({ type: 'timestamptz', nullable: true })
+    completedDate: Date | null;
+
+    @Column('text', {
+        nullable: true
+    })
+    notes?: string;
+
+  
+
+    
+
+}
